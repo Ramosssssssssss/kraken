2d92091b41ed73cfd62c7db03f6c13ea65da84cd
Ramosssssssssss <dnaps88@gmail.com>
2025-09-30 09:00:13 -0600

    s

--- app/detalle-orden/page.tsx
+++ app/detalle-orden/page.tsx
@@ -1,9 +1,10 @@
+// app/detalle-orden/page.tsx
 import { Suspense } from "react"
 import DetalleOrdenClient from "./DetalleOrdenClient"
 
 export default function Page() {
   return (
-    <Suspense fallback={<div>Cargando orden...</div>}>
+    <Suspense fallback={<div>Cargando...</div>}>
       <DetalleOrdenClient />
     </Suspense>
   )
